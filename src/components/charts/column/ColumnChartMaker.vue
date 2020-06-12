776dab6a2d40a5f805ae7cb9df204f6d4c753e00
rowatk <rowaneatkinson@gmail.com>
2020-06-12 15:55:25 -0500

    changed structure of charts by using a Base Mixin

--- src/components/charts/column/ColumnChartMaker.vue
+++ src/components/charts/column/ColumnChartMaker.vue
@@ -8,21 +8,23 @@
 </template>
 
 <script>
+import ChartMaker from "@/components/charts/ChartMaker.js"
 import ColumnChart from "@/components/charts/column/ColumnChart.vue";
-import { merge } from 'lodash'
+// import { merge } from 'lodash'
 // backgroundColor: ["#3498DB", "#138D75", "#6C3483"],
 export default {
   components: {
     ColumnChart,
   },
+  mixins: [ChartMaker],
   props: ["data"], // user defined properties
   data() {
+    console.log("COLOOOOORS:", this.backgroundColors)
     return {
       chartdata: {
         datasets: [
           {
-            backgroundColor: ["#2B2D42", "#39B54A", "#3F78BD", "#7AB6FF", "#DBECF8", "#D90429"],
-            borderColor: ["#000000"],
+            borderColor: "#000000",
             borderWidth: 1,
             hoverBackgroundColor: "#8D99AE",
             hoverBorderColor: "#FFFFFF",
@@ -34,13 +36,6 @@ export default {
         categoryPercentage: 0.8,
         animation: {
           animateScale: true
-        },
-        title: {
-          display: true,
-          text: this.splitTitle(),
-          position: 'bottom',
-          padding: 30,
-          fontColor: '#2B2D42'
         }
       }
     };
@@ -48,47 +43,8 @@ export default {
   mounted() {
     console.log("USER CONFIG")
     console.log(this.data)
-  },
-  methods: {
-    getRandomInt() {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
-    },
-
-    splitTitle() {
-      if (this.data.title) {
-        const limit = 7
-        const words = this.data.title.split(' ')
-        let lines = []
-        for(var i = 0; i<words.length; i+=limit){
-          lines.push(words.slice(i, i+limit).join(' '))
-        }
-        console.log(lines)
-        return lines
-      } else {
-        return ''
-      }
-    },
-
-    /**
-     * merge user chartdata and developer chartdata. preference placed on user chartdata
-     * @return {Object} merged chartdata object for chart
-     */
-    mergeProps() {
-      let mp = merge(this.chartdata || {}, this.data.properties)
-      console.log("MERGED")
-      console.log(mp)
-      return mp
-    },
-
-    /**
-     * merge user options and developer options. preference placed on user options
-     * @return {Object} merged options object for chart
-     */
-    mergeOptions() {
-
-      return merge(this.options || {}, this.data.options)
-    }
-  },
+    console.log("COLUMN COLOOOOORS:", this.backgroundColors)
+  }
 };
 </script>
 
