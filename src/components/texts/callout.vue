3e98ac6f338de94fc5505c409e68a136e9f1cd45
shakeanehinds <44011884+shakeanehinds@users.noreply.github.com>
2020-07-22 21:04:24 -0500

    fixed typos

--- src/components/texts/callout.vue
+++ src/components/texts/callout.vue
@@ -35,9 +35,9 @@ p {
   /* background-color: green; */
   display: grid;
   grid-template-columns: 1fr;
-  width: 80%;
+  width: 100%;
   background-color: rgba(255, 255, 255, 0.555);
   padding: 1em;
-  margin: 1em;
+  margin: 0.5em;
 }
 </style>
